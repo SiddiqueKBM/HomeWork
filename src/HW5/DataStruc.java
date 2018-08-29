faeebcfce148b19e5661f0ab629f115916d3f3e9
SiddiqueKBM <kbmsiddique.official@gmail.com>
2018-08-28 20:51:58 -0400

    HW5

---
+++ src/HW5/DataStruc.java
@@ -0,0 +1,11 @@
+package HW5;
+
+public class DataStruc {
+
+    static int a = 100;
+
+    public static void pnt(){
+        System.out.println(a);
+
+    }
+}
---
+++ src/TestDataStruc.java
@@ -0,0 +1,10 @@
+import HW5.DataStruc;
+
+public class TestDataStruc {
+    public static void main(String[] args) {
+
+        DataStruc.pnt();
+
+
+    }
+}
